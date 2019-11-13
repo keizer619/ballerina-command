15f17d9701ad9f08f32e9723cc650fa94a8540f4
Tharik Kanaka <tharik.kanaka@gmail.com>
2019-11-13 17:37:08 +0530

    Include the initial content

---
+++ src/main/java/tool/cmd/ListCommand.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2019, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tool.cmd;
+
+import picocli.CommandLine;
+import tool.BLauncherCommand;
+import tool.BallerinaCliCommands;
+import tool.util.ToolUtil;
+
+import java.io.PrintStream;
+import java.util.List;
+
+/**
+ * This class represents the "Update" command and it holds arguments and flags specified by the user.
+ *
+ * @since 1.0
+ */
+@CommandLine.Command(name = "list", description = "List Ballerina Distributions")
+public class ListCommand extends Command implements BLauncherCommand {
+
+    @CommandLine.Parameters(description = "Command name")
+    private List<String> listCommands;
+
+    @CommandLine.Option(names = {"--help", "-h", "?"}, hidden = true)
+    private boolean helpFlag;
+
+    @CommandLine.Option(names = {"--local"}, hidden = true)
+    private boolean localFlag;
+
+    private CommandLine parentCmdParser;
+
+    public ListCommand(PrintStream printStream) {
+        super(printStream);
+    }
+
+    public void execute() {
+        if (helpFlag) {
+            printUsageInfo(BallerinaCliCommands.LIST);
+            return;
+        }
+
+        if (listCommands == null) {
+            ToolUtil.listDistributions(getPrintStream(), localFlag);
+            return;
+        } else if (listCommands.size() > 1) {
+            //  throw LauncherUtils.createUsageExceptionWithHelp("too many arguments given");
+        }
+
+        String userCommand = listCommands.get(0);
+        if (parentCmdParser.getSubcommands().get(userCommand) == null) {
+            // throw LauncherUtils.createUsageExceptionWithHelp("unknown command " + userCommand);
+        }
+    }
+
+    @Override
+    public String getName() {
+        return BallerinaCliCommands.LIST;
+    }
+
+    @Override
+    public void printLongDesc(StringBuilder out) {
+
+    }
+
+    @Override
+    public void printUsage(StringBuilder out) {
+        out.append("  ballerina dist list\n");
+    }
+
+    @Override
+    public void setParentCmdParser(CommandLine parentCmdParser) {
+        this.parentCmdParser = parentCmdParser;
+    }
+}
